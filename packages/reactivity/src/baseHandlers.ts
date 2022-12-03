dc92e8c0e55746bcc3b070116d89e461bed46fa5
shideshan <307042873@qq.com>
2022-12-04 06:23:13 +0800

    :bug: fix: 深层属性递归reactive

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -1,5 +1,6 @@
-import { ReactiveFlags } from "./reactive";
+import { reactive, ReactiveFlags } from "./reactive";
 import { track, trigger } from "./effect";
+import { isObject } from "@vue/shared";
 
 export const mutableHandlers = {
   // receiver 是当前的代理对象
@@ -21,7 +22,13 @@ export const mutableHandlers = {
      * 【解决方式】而改成 Reflect.get(target, key, receiver) 后，this 指向 receiver，this.name 即 personProxy.name，依然会触发响应式；
      */
     track(target, key);
-    return Reflect.get(target, key, receiver); // 【使用 Reflect.get 处理了 target 内部的 this 指向问题】
+    let r = Reflect.get(target, key, receiver); // 【使用 Reflect.get 处理了 target 内部的 this 指向问题】
+
+    // 取值的时候，如果属性依然是对象，才对该属性递归使用reactive，相较于vue2性能更好
+    if (isObject(r)) {
+      return reactive(r);
+    }
+    return r;
   },
   set(target, key, value, receiver) {
     let oldValue = target[key];
