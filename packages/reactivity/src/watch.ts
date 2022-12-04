a80b3d23c19b62a56c119c5e6c73fcace26c326f
shideshan <307042873@qq.com>
2022-12-04 10:01:06 +0800

    :sparkles: feat: watchEffect API

--- packages/reactivity/src/watch.ts
+++ packages/reactivity/src/watch.ts
@@ -17,7 +17,8 @@ function traverse(source, s = new Set()) {
   }
   return source;
 }
-export function watch(source, cb, options) {
+
+function doWatch(source, cb, { immediate } = {} as any) {
   let getter;
   // 【关键】watch第一个参数是对象时，该对象必须是响应式的
   if (isReactive(source)) {
@@ -29,15 +30,32 @@ export function watch(source, cb, options) {
   let oldValue;
   // watcher effect 的依赖变化时，就会执行scheduler（即job）
   const job = () => {
-    // 在 scheduler 中调用 cb
-    let newValue = effect.run(); // 再次执行effect.run()，即再次执行getter，拿到新值
-    cb(newValue, oldValue);
-    oldValue = newValue;
+    // 2.1. 如果是watch API，在 scheduler 中调用 cb
+    if (cb) {
+      let newValue = effect.run(); // 再次执行effect.run()，即再次执行getter，拿到新值
+      cb(newValue, oldValue);
+      oldValue = newValue;
+    } else {
+      // 2.2. 如果是 watchEffect API，在 scheduler 中再次执行 effect.run
+      effect.run();
+    }
   };
   const effect = new ReactiveEffect(getter, job);
   // immediate为true会默认先执行一次回调
-  if (options?.immediate) {
+  if (immediate) {
     return job();
   }
-  oldValue = effect.run(); // 执行getter，收集依赖
+  oldValue = effect.run(); // 1. 执行getter，收集依赖
+}
+
+/**
+ * watch 和 watchEffect的区别：
+ * 1. watch 就是一个 effect + 自定义scheduler（在scheduler中执行回调cb）
+ * 2. watchEffect 就是一个 effect（只是在scheduler中执行 effect.run() 而已）
+ */
+export function watch(source, cb, options) {
+  doWatch(source, cb, options);
+}
+export function watchEffect(effect, options) {
+  doWatch(effect, null, options);
 }
