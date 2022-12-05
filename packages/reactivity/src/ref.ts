52696aa3ab3376f8f5a0e2fd4bbcb0462324e090
shideshan <307042873@qq.com>
2022-12-05 11:04:36 +0800

    :sparkles: feat: add toRef、toRefs、proxyRefs API

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -3,13 +3,12 @@ import { reactive } from "./reactive";
 import { activeEffect, trackEffects, triggerEffects } from "./effect";
 
 export function ref(value) {
-  return new RefImp(value);
+  return new RefImpl(value);
 }
-
 function toReactive(value) {
   return isObject(value) ? reactive(value) : value;
 }
-class RefImp {
+class RefImpl {
   dep = undefined;
   _value;
   __v_isRef = true;
@@ -33,4 +32,49 @@ class RefImp {
       triggerEffects(this.dep);
     }
   }
+}
+
+export function toRef(target, key) {
+  return new ObjectRefImpl(target, key);
+}
+class ObjectRefImpl {
+  __v_isRef = true;
+  constructor(public _object, public _key) {}
+  get value() {
+    return this._object[this._key];
+  }
+  set value(newValue) {
+    this._object[this._key] = newValue;
+  }
+}
+
+// toRefs 本质是遍历对象，对每一个属性使用toRef进行代理
+export function toRefs(object) {
+  const ret = {};
+  for (let key in object) {
+    ret[key] = toRef(object, key);
+  }
+  return ret;
+}
+
+/**
+ * proxyRefs 用proxy代理一个对象
+ * 1. 如果对象属性是Ref类型，则将get和set都代理到该属性的 value 属性上
+ * 2. 如果对象属性不是Ref类型，则正常取值和设值
+ */
+export function proxyRefs(objectWithRefs) {
+  return new Proxy(objectWithRefs, {
+    get(target, key, receiver) {
+      let v = Reflect.get(target, key, receiver);
+      return v.__v_isRef ? v.value : v;
+    },
+    set(target, key, value, receiver) {
+      const oldValue = target[key];
+      if (oldValue.__v_isRef) {
+        oldValue.value = value;
+        return true;
+      }
+      return Reflect.set(target, key, value, receiver);
+    },
+  });
 }
